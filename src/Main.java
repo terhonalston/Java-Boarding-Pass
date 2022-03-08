bf82ed8bb393a84248a35f777798f8fb7f48227b
TeRhon Alston <Terhon.Alston@GenSpark.net>
2022-03-08 15:15:13 -0500

    Added board number generator. Started ticket class.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        HashSet<Integer> boardingPassList = new HashSet<>();
     }
-
-
+    public static int boardingPassNumber(HashSet<Integer>boardingPassList){
+        boolean numberNotAdded = true;
+        int boardingPassNumber = 0;
+        while(numberNotAdded) {
+            boardingPassNumber= (int) (100000+(Math.random()*999999));
+            while (!boardingPassList.contains(boardingPassNumber)) {
+                boardingPassList.add(boardingPassNumber);
+                numberNotAdded = false;
+            }
+        }
+        return boardingPassNumber;
+    }
 }
--- src/Ticket.java
+++ src/Ticket.java
@@ -1,29 +1,120 @@
 import java.util.Scanner;
 
 public class Ticket {
-    public static void main(String[] args) {
-        System.out.println("Enter Name: ");
-        Scanner scanner = new Scanner(System.in);
-        String inputName = scanner.nextLine();
-        System.out.println("Enter Email: ");
-        String inputEmail = scanner.nextLine();
-        System.out.println("Enter Phone Number: ");
-        String inputPhone = scanner.nextLine();
-        System.out.println("Enter Gender: ");
-        String inputGender = scanner.nextLine();
-        System.out.println("Enter Age: ");
-        String inputAge = scanner.nextLine();
-        System.out.println("Enter Boarding pass: ");
-        String inputPass = scanner.nextLine();
-        System.out.println("Enter Date: ");
-        String inputDate = scanner.nextLine();
-        System.out.println("Enter Orgin: ");
-        String inputOrigin = scanner.nextLine();
-        System.out.println("Enter Destination: ");
-        String inputDest = scanner.nextLine();
-        System.out.println("Enter Estimated time of arrival (ETA): ");
-        String inputETA = scanner.nextLine();
-        System.out.println("Enter Departure time: ");
-        String inputDeparture = scanner.nextLine();
-    }
+        Scanner scanner= new Scanner(System.in);
+
+        String inputName;
+        String inputEmail;
+        String inputPhone;
+        String inputGender;
+        String inputAge;
+        String inputPass;
+        String inputDate ;
+        String inputOrigin;
+        String inputDest;
+        String inputETA;
+        String inputDeparture;
+
+        public Ticket(String inputName, String inputEmail, String inputPhone, String inputGender,
+                      String inputAge, String inputPass, String inputDate, String inputOrigin, String inputDest,
+                      String inputETA, String inputDeparture) {
+                this.inputName = inputName;
+                this.inputEmail = inputEmail;
+                this.inputPhone = inputPhone;
+                this.inputGender = inputGender;
+                this.inputAge = inputAge;
+                this.inputPass = inputPass;
+                this.inputDate = inputDate;
+                this.inputOrigin = inputOrigin;
+                this.inputDest = inputDest;
+                this.inputETA = inputETA;
+                this.inputDeparture = inputDeparture;
+        }
+        public String getInputName() {
+                return inputName;
+        }
+
+        public void setInputName(String inputName) {
+                this.inputName = inputName;
+        }
+
+        public String getInputEmail() {
+                return inputEmail;
+        }
+
+        public void setInputEmail(String inputEmail) {
+                this.inputEmail = inputEmail;
+        }
+
+        public String getInputPhone() {
+                return inputPhone;
+        }
+
+        public void setInputPhone(String inputPhone) {
+                this.inputPhone = inputPhone;
+        }
+
+        public String getInputGender() {
+                return inputGender;
+        }
+
+        public void setInputGender(String inputGender) {
+                this.inputGender = inputGender;
+        }
+
+        public String getInputAge() {
+                return inputAge;
+        }
+
+        public void setInputAge(String inputAge) {
+                this.inputAge = inputAge;
+        }
+
+        public String getInputPass() {
+                return inputPass;
+        }
+
+        public void setInputPass(String inputPass) {
+                this.inputPass = inputPass;
+        }
+
+        public String getInputDate() {
+                return inputDate;
+        }
+
+        public void setInputDate(String inputDate) {
+                this.inputDate = inputDate;
+        }
+
+        public String getInputOrigin() {
+                return inputOrigin;
+        }
+
+        public void setInputOrigin(String inputOrigin) {
+                this.inputOrigin = inputOrigin;
+        }
+
+        public String getInputDest() {
+                return inputDest;
+        }
+
+        public void setInputDest(String inputDest) {
+                this.inputDest = inputDest;
+        }
+
+        public String getInputETA() {
+                return inputETA;
+        }
+
+        public void setInputETA(String inputETA) {
+                this.inputETA = inputETA;
+        }
+
+        public String getInputDeparture() {
+                return inputDeparture;
+        }
+
+        public void setInputDeparture(String inputDeparture) {
+                this.inputDeparture = inputDeparture;
+        }
 }
